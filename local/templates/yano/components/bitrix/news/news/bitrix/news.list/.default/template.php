1336f624dd7edd2e0cae900ea0be3e5e168fb473
osemeniha <semolga22@gmail.com>
2025-03-17 14:58:31 +0300

    feat: add component news and styles

---
+++ local/templates/yano/components/bitrix/news/news/bitrix/news.list/.default/template.php
@@ -0,0 +1,57 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<div class="press-center__articles press-center__articles--wide-list" data-target="view-more.container">
+
+<?if($arParams["DISPLAY_TOP_PAGER"]):?>
+ <?=$arResult["NAV_STRING"]?><br />
+<?endif;?>
+<? $firstElement = true;?>
+<?foreach($arResult["ITEMS"] as $arItem):?>
+ <?
+ $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+ $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+ ?>
+  <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
+   <?if ($firstElement):?>
+    <article class="news-important" id="<?=$this->GetEditAreaId($arItem['ID']);?>" style="background-image: url(<?echo $arItem["PREVIEW_PICTURE"]["SRC"]?>)">
+     <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>" class="news-important__link">
+      <h2 class="news-important__title">
+       <?echo $arItem["NAME"]?>
+      </h2>
+     </a>
+     <time class="news-important__publication-date" ><?echo $arItem["DATE_CREATE"]?></time>
+    </article>
+   <?else:?>
+    <article class="news news--wide">
+      <div class="news__publication-info">
+     <a href="<?echo $arItem["DETAIL_PAGE_URL"]?>" class="news__link">
+       <h3 class="news__title content-block">
+      <mark>
+       <?echo $arItem["NAME"]?>
+      </mark>
+      <span>
+        <?echo $arItem["PREVIEW_TEXT"]?>
+      </span>
+       </h3>
+     </a>
+     <time class="news__publication-date" ><?echo $arItem["DATE_CREATE"]?></time>
+      </div>
+      <div class="news__illustration" style="background-image: url(<?echo $arItem["PREVIEW_PICTURE"]["SRC"]?>)"></div>
+    </article>
+   <?endif;?>
+	<?$firstElement = false;?>
+  </div>
+<?endforeach;?>
+</div>
--- novosti/index.php
+++ novosti/index.php
@@ -2,118 +2,86 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Новости");
 ?>
-
 <section class="press-center" data-controller="view-more">
   <header class="press-center__header">
     <h1 class="light">Новости</h1>
   </header>
-  <div class="press-center__articles press-center__articles--wide-list" data-target="view-more.container">
-
-    <article class="news-important" style="background-image: url(<?=ASSET_PATH?>stub/1.jpg)">
-      <a href="/article.html" class="news-important__link">
-        <h2 class="news-important__title">
-          Стартовал прием заявок на&nbsp;I&nbsp;конкурс президентских
-          грантов 2020&nbsp;года! Желающие уже с&nbsp;1 марта
-          2020&nbsp;года получить грант Президента
-        </h2>
-      </a>
-      <time class="news-important__publication-date" datetime="2019-10-15">15 октября 2019</time>
-    </article>
-    <article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="/article.html" class="news__link">
-          <h3 class="news__title content-block">
-            <mark>
-              Общественная палата&nbsp;РФ будет продвигать методические
-              указания по&nbsp;госзакупкам услуг в&nbsp;сфере отлова
-              и&nbsp;содержания животных без владельцев.
-            </mark>
-            <span>
-              &nbsp;15&nbsp;ноября поговорили о&nbsp;проблемах
-              правоприменения закона &laquo;Об&nbsp;ответственном обращении
-              с&nbsp;животными&raquo;
-            </span>
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-11-08">8 ноября 2019</time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=ASSET_PATH?>stub/7.jpg)"></div>
-    </article>
-    <article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="/article.html" class="news__link">
-          <h3 class="news__title content-block">
-            <mark>
-              #ЩедрыйВторник пройдет 3&nbsp;декабря.
-            </mark>
-            <span>
-              В&nbsp;100&nbsp;странах, включая Россию, в&nbsp;четвертый раз
-              пройдет Международный день благотворительности
-              #ЩедрыйВторник. Миллионы людей объединяются для проведения
-              благотворительных мероприятий, вдохновляют других
-              на&nbsp;добрые дела.
-            </span>
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-10-24">24 октября 2019</time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=ASSET_PATH?>stub/8.jpg)"></div>
-    </article>
-    <article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="/article.html" class="news__link content-block">
-          <h3 class="news__title">
-            Заседание рабочей группы Общественной палаты Ямала
-            по&nbsp;формированию системы гуманного обращения
-            с&nbsp;животными
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-09-27">27 сентября 2019</time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=ASSET_PATH?>stub/9.jpg)"></div>
-    </article>
-    <article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="/article.html" class="news__link content-block">
-          <h3 class="news__title content-block">
-            <mark>
-              На&nbsp;Ямале реализуется общественно-образовательный проект
-              &laquo;Ямальские молодёжные инициативы&raquo;.
-            </mark>
-            <span>
-              Его участники могут получить гранты в&nbsp;размере 200, 150
-              и&nbsp;100&nbsp;тыс. рублей на&nbsp;реализацию своего
-              социально-ориентированного проекта
-            </span>
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-11-14">14 ноября 2019</time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=ASSET_PATH?>stub/10.jpg)"></div>
-    </article>
-    <article class="news news--wide">
-      <div class="news__publication-info">
-        <a href="/article.html" class="news__link content-block">
-          <h3 class="news__title content-block">
-            <mark>
-              Форум добровольцев Ямала соберет более 300&nbsp;участников.
-            </mark>
-            <span>
-              В&nbsp;Новом Уренгое 13&nbsp;и&nbsp;14&nbsp;декабря пройдет
-              форум добровольцев Ямала. Это долгожданное событие для
-              волонтерского движения автономного округа
-            </span>
-          </h3>
-        </a>
-        <time class="news__publication-date" datetime="2019-11-14">14 ноября 2019</time>
-      </div>
-      <div class="news__illustration" style="background-image: url(<?=ASSET_PATH?>stub/4.jpg)"></div>
-    </article>
-  </div>
-  <div class="grid-container">
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news",
+	"news",
+	Array(
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
+		"DETAIL_DISPLAY_TOP_PAGER" => "N",
+		"DETAIL_FIELD_CODE" => array("",""),
+		"DETAIL_PAGER_SHOW_ALL" => "Y",
+		"DETAIL_PAGER_TEMPLATE" => "",
+		"DETAIL_PAGER_TITLE" => "Страница",
+		"DETAIL_PROPERTY_CODE" => array("",""),
+		"DETAIL_SET_CANONICAL_URL" => "N",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "17",
+		"IBLOCK_TYPE" => "news",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"LIST_ACTIVE_DATE_FORMAT" => "j F Y",
+		"LIST_FIELD_CODE" => array("NAME","PREVIEW_TEXT","PREVIEW_PICTURE","DATE_CREATE",""),
+		"LIST_PROPERTY_CODE" => array("",""),
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Новости",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"SEF_FOLDER" => "/novosti/",
+		"SEF_MODE" => "Y",
+		"SEF_URL_TEMPLATES" => Array("detail"=>"#ELEMENT_CODE#/","news"=>"","section"=>""),
+		"SET_LAST_MODIFIED" => "N",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"USE_CATEGORIES" => "N",
+		"USE_FILTER" => "N",
+		"USE_PERMISSIONS" => "N",
+		"USE_RATING" => "N",
+		"USE_REVIEW" => "N",
+		"USE_RSS" => "N",
+		"USE_SEARCH" => "N",
+		"USE_SHARE" => "N"
+	)
+);?>
+<div class="grid-container">
     <a class="press-center__view-more button button--inverted" href="press-center.html" data-target="view-more.button"
       data-action="view-more#load">Показать более ранние материалы</a>
   </div>
 </section>
-
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
